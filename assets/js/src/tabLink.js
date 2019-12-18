a833ae200ffd3db4f31dfc2755c3f96cd1954f15
wdzajicek <tig.welder4@gmail.com>
2019-12-18 10:27:35 -0600

    Delete old `/assets/js/scripts/` dir. Add GED page to `tabLink.js`...
    Make `tabLink.js` functions fire on `.../programs/ged/` page too.

--- assets/js/src/tabLink.js
+++ assets/js/src/tabLink.js
@@ -1,11 +1,10 @@
 // Provide the ability to activate a tab from the URL (e.g. 'http://localhost:3000/alumni/distinguished-alumni-award/#donald-rehmer' makes the #donald-rehmer tab active).
-const DISTINGUISHED_ALUMNI_OR_ALUMNI_STORIES_REGEX = /programs\/community-literacy/g;
+const GED_OR_COMMUNITY_LITERACY_PAGE_REGEX = /(\/programs\/community-literacy|\/programs\/ged)/g;
 
 function makeTabsLinkable() {
-  let initialURL = location.href;
-  const userIsNotOnCorrectPage = initialURL.search(DISTINGUISHED_ALUMNI_OR_ALUMNI_STORIES_REGEX) === -1;
+  let url = location.href;
 
-  if ( userIsNotOnCorrectPage )
+  if ( url.search(GED_OR_COMMUNITY_LITERACY_PAGE_REGEX) === -1 )
     return;
 
   if (location.hash) {
